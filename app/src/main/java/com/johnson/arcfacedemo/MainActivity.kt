221120d02134a12ed00e180df04dbd1bc0eb2cfb
Johnson <poiuytrengo@qq.com>
2019-07-26 17:02:29 +0800

    Project Init

---
+++ app/src/main/java/com/johnson/arcfacedemo/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.johnson.arcfacedemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/johnson/arcfacedemo/common/Constants.java
@@ -0,0 +1,6 @@
+package com.johnson.arcfacedemo.common;
+
+public class Constants {
+    public static final String APP_ID = "DvuS2T7Z6gPg5NZo3BhgpLXz2QaGnisbcPaaoGXq4rcw";
+    public static final String SDK_KEY = "CZc8a5Ta7K1YmrPAqFRkAHkvq7qf724bqWhq8E1dsieK";
+}
